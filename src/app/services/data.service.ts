69ff73a5d1c80128fc0800c2fccdf00bcd12d1b4
Queenie Jaybee <qjgmaniquis@gmail.com>
2025-10-22 04:29:57 +0800

    feat: implement DataService API integration with list method

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,11 +1,26 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../models/user.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  private readonly http = inject(HttpClient);
+  
+  private readonly apiBase = 'https://fulcrumdashboard-8358.restdb.io/rest';
+  private readonly apiKey = '68f7e9f47f34edcb46200b8e';
+  private readonly defaultCollection = 'db-users';
 
-  constructor() {
-    console.log('DataService initialized');
+  private headers(): HttpHeaders {
+    return new HttpHeaders({
+      'x-apikey': this.apiKey
+    });
+  }
+
+  list(): Observable<User[]> {
+    const url = `${this.apiBase.replace(/\/$/, '')}/${this.defaultCollection}`;
+    return this.http.get<User[]>(url, { headers: this.headers() });
   }
 }
